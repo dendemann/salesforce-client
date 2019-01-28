426dc782a81aff8f204258b562b7a1af4c86c532
Dennis Holst <dholst@rapidsoft.de>
2019-01-28 17:24:08 +0100

    Provide IDE auto completion help for resource factory

---
+++ .phpstorm.meta.php/client.meta.php
@@ -0,0 +1,12 @@
+<?php
+namespace PHPSTORM_META {
+
+    override(\WakeOnWeb\SalesforceClient\ClientInterface::getResource(0),
+        map([
+            'sobjects' => '\WakeOnWeb\SalesforceClient\REST\Resource\SObjects',
+            'query' => '\WakeOnWeb\SalesforceClient\REST\Resource\Query',
+            'queryAll' => '\WakeOnWeb\SalesforceClient\REST\Resource\QueryAll'
+        ])
+    );
+
+}
